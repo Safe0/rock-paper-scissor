f240864ff742d46844c8dee18f756f147a0b405f
Jim <serra.jim06@gmail.com>
2025-12-11 16:28:28 +0100

    Add game button + Hand option

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import { Scoreboard } from './components/molecules/Scoreboard/Scoreboard';
 import { GridContainer } from './components/templates/GridContainer/GridContainer';
 import { GridItem } from '@templates/GridItem/GridItem';
 import { Stack } from './components/templates/Stack/Stack';
-import { ButtonIcon } from './components/atoms/ButtonIcon/ButtonIcon';
+import { Button } from './components/atoms/Button/Button';
 import scissorsImg from './assets/scissors.jpg';
+import { HandOption } from './components/organisms/HandOption/HandOption';
+import { useState } from 'react';
 
 export const App = () => {
+  const [isGame, setIsGame] = useState(false);
   return (
     <GridContainer spacing={2}>
       <GridItem xs={12}>
@@ -15,19 +18,25 @@ export const App = () => {
         </Stack>
       </GridItem>
       <GridItem xs={12} lg={4}>
-        <Scoreboard
-          title="PvP"
-          label1="Player 1"
-          label2="Player 2"
-          score1={0}
-          score2={0}
-        />
-      </GridItem>
-      <GridItem xs={12} lg={8}>
-        <Stack justifyContent="center" alignItems="center">
-          <ButtonIcon img={scissorsImg} label={'Scissors'} />
+        <Stack spacing={2}>
+          <Scoreboard
+            title="PvP"
+            label1="Player 1"
+            label2="Player 2"
+            score1={0}
+            score2={0}
+          />
+          {!isGame && <Button onClick={() => setIsGame(true)} label="Play" />}
         </Stack>
       </GridItem>
+      {isGame && (
+        <GridItem xs={12} lg={8}>
+          <HandOption
+            onClick={(k: string) => console.log(k)}
+            title={'Player 1'}
+          />
+        </GridItem>
+      )}
     </GridContainer>
   );
 };
--- src/components/templates/Stack/Stack.tsx
+++ src/components/templates/Stack/Stack.tsx
@@ -1,40 +1,40 @@
 import styles from './Stack.module.scss';
 
 interface StackProps {
-  children: React.ReactNode;
-  direction?: 'row' | 'column';
-  spacing?: number;
-  justifyContent?:
+    children: React.ReactNode;
+    direction?: 'row' | 'column';
+    spacing?: number;
+    justifyContent?:
     | 'start'
     | 'center'
     | 'end'
     | 'space-between'
     | 'space-around'
     | 'space-evenly';
-  alignItems?: 'start' | 'center' | 'end' | 'stretch';
-  wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
+    alignItems?: 'start' | 'center' | 'end' | 'stretch';
+    wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
 }
 
 export const Stack = ({
-  children,
-  direction = 'column',
-  spacing = 0,
-  justifyContent = 'start',
-  alignItems = 'stretch',
-  wrap = 'nowrap',
+    children,
+    direction = 'column',
+    spacing = 0,
+    justifyContent = 'start',
+    alignItems = 'stretch',
+    wrap = 'nowrap',
 }: StackProps) => {
-  return (
-    <div
-      className={styles.stack}
-      style={{
-        flexDirection: direction,
-        gap: `${spacing * 8}px`, // spacing MUI-like
-        justifyContent,
-        alignItems,
-        flexWrap: wrap,
-      }}
-    >
-      {children}
-    </div>
-  );
+    return (
+        <div
+            className={styles.stack}
+            style={{
+                flexDirection: direction,
+                gap: `${spacing * 8}px`,
+                justifyContent,
+                alignItems,
+                flexWrap: wrap,
+            }}
+        >
+            {children}
+        </div>
+    );
 };
